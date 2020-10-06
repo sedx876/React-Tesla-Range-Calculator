9ade59d2ec8306ec95ed1a2bf9ae75a802776a4c
sedx876 <swatkins876@gmail.com>
2020-10-06 08:20:05 -0400

    added teslacar and css added prop-types dependency

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, {Component} from 'react';
 import './styles/App.css';
 import Header from './components/Header/Header'
+import TeslaBattery from './containers/TeslaBattery'
 
 class App extends Component {  
   render() {
     return (      
       <div>        
       <Header /> 
+      <TeslaBattery />
       </div>    
       ); 
     }}
